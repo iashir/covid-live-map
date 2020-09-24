a2d9a6ba0372e0f3b8ce018e3bd4d65e4fd7110e
ilyas <ilyas.ashir96@gmail.com>
2020-09-24 17:32:24 -0400

    test

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const path = require("path");
 const express = require("express");
 const app = express();
-const publicPath = path.join(__dirname, "..", "public");
+
 const port = process.env.PORT || 5000;
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
