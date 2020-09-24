364e3f4bd7405d260deba5fc272240bacd5f5e4c
ilyas <ilyas.ashir96@gmail.com>
2020-09-24 17:45:22 -0400

    test

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 5000;
-app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(express.static("./client/src/public"));
 
 app.get("*", (req, res) => {
