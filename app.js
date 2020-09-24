83602277cf333fb843a9b45307cce8ff30ed01ce
ilyas <ilyas.ashir96@gmail.com>
2020-09-24 17:56:21 -0400

    test commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 app.use(express.static("./client/src/public"));
-
+app.use(cors());
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
